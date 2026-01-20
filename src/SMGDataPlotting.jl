428b36ceb1785790d8c04185a972a8e4a75feca1
Nick Owens <owensnick@gmail.com>
2026-01-20 20:36:15 +0000

    setup package

--- src/SMGDataPlotting.jl
+++ src/SMGDataPlotting.jl
@@ -1,5 +1,18 @@
 module SMGDataPlotting
 
-# Write your package code here.
+using SMGReader
+using DataFrames, DataFramesMeta, CSV
+using CairoMakie
+
+using GenomicFeatures
+
+export  pilereads, binvector, firepile, collectfire, assignlevels, loadbed, bedintervals, bedplot, loadgenemodels
+
+include("data.jl")
+include("utils.jl")
+include("genome.jl")
+include("stencillingplots.jl")
+
+
 
 end
