f2df54cebc33fac4190144014bebd1a2ac740bd5
Nick Owens <owensnick@gmail.com>
2026-02-25 19:38:00 +0000

    added exports

--- src/SMGDataPlotting.jl
+++ src/SMGDataPlotting.jl
@@ -1,3 +1,9 @@
+"""
+    SMGDataPlotting
+
+A package for plotting single molecule genomics data, including fiber-seq and direct RNA-seq from nanopore.
+It provides tools for visualizing read pileups, MSPs (Methylation Specific Patches) / FIRE (Fiber-seq inferred regulatory elements) elements, and other genomic data.
+"""
 module SMGDataPlotting
 
 using SMGReader
@@ -6,7 +12,7 @@ using CairoMakie
 
 using GenomicFeatures
 
-export  pilereads, binvector, firepile, collectfire, assignlevels, loadbed, bedintervals, bedplot, loadgenemodels
+export pilereads, binvector, firepile, collectfire, assignlevels, loadbed, bedintervals, bedplot, loadgenemodels, filterreads, combinereads, plotgenemodels, fireplot, modplot
 
 include("data.jl")
 include("utils.jl")
